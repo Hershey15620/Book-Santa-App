6705133c8b85f69d7db68a396a1301fa78ab913a
Hershey15620 <67335475+Hershey15620@users.noreply.github.com>
2021-03-05 14:00:40 -0500

    Add files via upload

---
+++ AppStackNavigator.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import { createStackNavigator } from 'react-navigation-stack';
+
+import BookDonateScreen from '../screens/BookDonateScreen';
+import RecieverDetailsScreen  from '../screens/RecieversDetailsScreen';
+
+export const AppStackNavigator=createStackNavigator({
+    BookDonateList : {
+        screen : BookDonateScreen,
+        navigationOptions:{
+          headerShown : false
+        }
+      },
+      RecieverDetails : {
+        screen : RecieverDetailsScreen,
+        navigationOptions:{
+          headerShown : false
+        }
+      },
+    
+    },
+    {
+intialRouteName: 'BookDonateList'
+})
